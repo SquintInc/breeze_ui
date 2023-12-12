66a32d320ef0e658472af5d7c001f4ef101bec54
Kevin Xiao <xlzqwerty1@gmail.com>
2023-12-12 12:39:39 -0800

    Export text span

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -3,3 +3,4 @@ export 'package:tailwind_elements/widgets/div.dart';
 export 'package:tailwind_elements/widgets/sized_box.dart';
 export 'package:tailwind_elements/widgets/style.dart';
 export 'package:tailwind_elements/widgets/text.dart';
+export 'package:tailwind_elements/widgets/text_span.dart';
