11c7f67f7e4c1d4bbe416031a925bed5d08a6521
Kevin Xiao <xlzqwerty1@gmail.com>
2024-01-01 21:33:34 -0500

    Fix circle radius extension for infinite constraints

--- lib/widgets/extensions/extensions.dart
+++ lib/widgets/extensions/extensions.dart
@@ -1,9 +1,23 @@
 import 'dart:math';
 
 import 'package:flutter/widgets.dart';
+import 'package:tailwind_elements/config/options/borders/border_radius.dart';
 
 extension RadiusExt on BoxConstraints {
-  double get circleRadius => (min(minWidth, minHeight) / 2).ceilToDouble();
+  double get circleRadius {
+    final bool hasInfiniteHeight = maxHeight.isInfinite;
+    final bool hasInfiniteWidth = maxWidth.isInfinite;
+    if (hasInfiniteWidth && hasInfiniteHeight) {
+      return TwBorderRadius.fullRadiusPx;
+    }
+    if (hasInfiniteWidth) {
+      return (maxHeight / 2).ceilToDouble();
+    }
+    if (hasInfiniteHeight) {
+      return (maxWidth / 2).ceilToDouble();
+    }
+    return (min(minWidth, minHeight) / 2).ceilToDouble();
+  }
 }
 
 extension BoxConstraintsExtension on BoxConstraints {
